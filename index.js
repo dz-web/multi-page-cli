1f059334019564b281fc692038ed437ae4fa1803
yeluoqiuzhi <827884374@qq.com>
2017-10-26 10:12:07 +0800

    first release

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const { trimEnd } = require('lodash');
+const { renderFile, render } = require('ejs');
+const { pascalCase } = require('change-case');
 
 const config = require(path.join(process.cwd(), '.mpcli.config.js'));
 const cwd = process.cwd();
@@ -37,7 +39,15 @@ const cwd = process.cwd();
           name: 'entry',
           message: 'entry path',
           default: function(answers) {
-            return `${config.entriesPath}/${answers.name}/${answers.name}`;
+            return `${config.entries.directory}/${answers.name}/${answers.name}.jsx`;
+          }
+        },
+        {
+          type: 'input',
+          name: 'view',
+          message: 'views path',
+          default: function(answers) {
+            return `${config.views.directory}/${answers.name}/${answers.name}.jsx`;
           }
         },
         {
@@ -47,7 +57,12 @@ const cwd = process.cwd();
           choices: config.templates
         }
       ]).then(function(answers) {
+        // append entry config
         appendEntry(answers.name, answers.title, answers.entry, answers.template)
+        // create entry
+        createComponent(answers.entry, answers.name, config.entries.template)
+        // create views
+        createComponent(answers.view, answers.name, config.views.template, true)
       });
     })
     .help()
@@ -57,13 +72,13 @@ const cwd = process.cwd();
 function appendEntry(...args) {
   let rawConfigText = fs.readFileSync(path.join(cwd, config.file), 'utf-8');
   
-  // 发布时，修改为输出至文件
-  console.log(generate.apply(rawConfigText, args));
+  fs.writeFileSync(config.file, generate.apply(rawConfigText, args));
 }
 
 function generate(name, title, entry, template) {
   return this.replace(/html:\s*\[((?:.|[\r\n])*)\]/, (match, entriesText) => {
     const temp = `html: [${trimEnd(entriesText)}
+    // ${title}
     {
       name: '${name}',
       title: '${title}',
@@ -74,4 +89,37 @@ function generate(name, title, entry, template) {
 
     return temp;
   });
+}
+
+function createComponent(location, name, templateFile, scss) {
+  const dir = path.dirname(location);
+  if (fs.existsSync(dir)) {
+    const message = `directory ${dir} already exists`;
+    console.log(chalk.red(message));
+    throw new Error(message);
+  }
+  fs.mkdirSync(dir);
+  renderFile(path.join(cwd, templateFile), {
+    pascal: pascalCase(name),
+    kebab: name
+  }, function(err, str) {
+    if (err) throw err;
+
+    fs.writeFileSync(location, str);
+  });
+
+  // 创建样式文件
+  if (scss) {
+    const stylesheet = render(
+`.<%- kebab %> {
+  background: lightcoral;
+}
+`,
+    {
+      pascal: pascalCase(name),
+      kebab: name
+    });
+    
+    fs.writeFileSync(`${dir}/${name}.scss`, stylesheet);
+  }
 }
